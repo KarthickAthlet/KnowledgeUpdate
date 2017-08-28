94201291b1d51432b450b69fb7b4aa9ead99e95f
karthikeyan N P <karthick.psgtec12@gmail.com>
2017-08-29 01:21:37 +0530

    1) commiting Hello Component lifecycle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
+/**
+ * Components Life cycle method:
+ * 
+ * Three phases:
+ * 
+ * Mount Phase:
+ *      Initial render:
+ *      1) constructor()
+ *      2) componentwillMount()
+ *      3) render()
+ *      4) componentDidMount() 
+ *          - right place for timers, ajax
+ * 
+ * Update Phase:
+ *      Once Ui rendered, triggered by events:
+ *      1) componentWillReceiveProps()
+ *          - Dynamic props gets passed - called
+ *      2) shouldComponentUpdate()
+ *          - prevent your component update (not much useful)
+ *      3) componentWillUpdate()
+ *      4) Render()
+ *      5) ComponentDidUpdate()
+ * 
+ *  unmount phase:
+ *      when DOM element is removed from the DOM tree 
+ *      1) componentWillUnmount()
+ * 
+ */
+
+
+// Imports for React and React DOM initialization
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export class ComponentLifecycle extends React.Component {
+
+    render(){
+        return <div>Hello ComponentLifecycle</div>;
+    }
+} 
+
+ReactDOM.render(<ComponentLifecycle/>, document.getElementById('root'));
